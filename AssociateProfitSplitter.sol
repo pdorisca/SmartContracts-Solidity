39a3f78edb5424673dfbe72b1ec6e2bea14cf8bf
Phara Dorisca <66986114+pdorisca@users.noreply.github.com>
2020-11-23 16:41:39 -0500

    Update AssociateProfitSplitter.sol

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.5.0;
 // lvl 1: equal split
 contract AssociateProfitSplitter {
     // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    address payable employee_one; // 0xB8f9EE94cAE293D1C9836948a342447159eFd235
+    address payable employee_two; // 0xC20D4A0B5DB447c40f9f8ED53e9f454705dF602F
+    address payable employee_three; // 0x7E477904ABe1a220ba281c30B66CF6cE66D80F53
 
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
@@ -16,17 +19,20 @@ contract AssociateProfitSplitter {
 
     function deposit() public payable {
         // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
+        uint amount = msg.value / 3; 
 
         // @TODO: Transfer the amount to each employee
-        // Your code here!
+        employee_one.transfer(amount);
+        employee_two.transfer(amount); 
+        employee_three.transfer(amount);
 
         // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+         msg.sender.transfer(msg.value - (amount *3));
     }
 
     function() external payable {
         // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
+        deposit();
+        
     }
 }
